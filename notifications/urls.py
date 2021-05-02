949b7a4a7493772899300caee976a61b7cef10e8
amir <amirreza.m.saki@gmail.com>
2021-05-02 13:18:50 +0430

    Fix mistaking in using right variable name.

--- notifications/urls.py
+++ notifications/urls.py
@@ -15,9 +15,9 @@ else:
 urlpatterns = [
     pattern(r'^mark-all-as-read/$', views.mark_all_as_read, name='mark_all_as_read'),
     pattern(r'^mark-all-as-unread/$', views.mark_all_as_unread, name='mark_all_as_unread'),
-    pattern(r'^mark-as-read/(?P<slug>\d+)/$', views.mark_as_read, name='mark_as_read'),
-    pattern(r'^mark-as-unread/(?P<slug>\d+)/$', views.mark_as_unread, name='mark_as_unread'),
-    pattern(r'^delete/(?P<slug>\d+)/$', views.delete, name='delete'),
+    pattern(r'^mark-as-read/(?P<notification_id>\d+)/$', views.mark_as_read, name='mark_as_read'),
+    pattern(r'^mark-as-unread/(?P<notification_id>\d+)/$', views.mark_as_unread, name='mark_as_unread'),
+    pattern(r'^delete/(?P<notification_id>\d+)/$', views.delete, name='delete'),
     pattern(r'^unread_count/$', views.live_unread_notification_count, name='live_unread_notification_count'),
     pattern(r'^all_count/$', views.live_all_notification_count, name='live_all_notification_count'),
     pattern(r'^unread_list/$', views.live_unread_notification_list, name='live_unread_notification_list'),
